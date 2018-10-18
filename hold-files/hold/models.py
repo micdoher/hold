07959dbb7d9d1ee9ac338c48903b2f8f751d3745
micdoher <micdoher@cisco.com>
2018-10-18 15:30:56 +0200

    changing file structure

---
+++ hold-files/hold/models.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from sqlalchemy import desc
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from hold import db
+
+
+eaten = ['chillies','peppers','sweets','avocados', 'banana', 'milk', 'bread', 'nutella', 'olive oil', 'orange juice', 'pancakes']
+mood = ['high', 'low', 'energetic', 'tired']
+
+
+class Bookmark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    food_word_1 = db.Column(db.Text, nullable=False)
+    food_word_2 = db.Column(db.Text, nullable=False)
+    food_word_3 = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    amount_1 = db.Column(db.Integer, nullable=False)
+    amount_2 = db.Column(db.Integer, nullable=False)
+    amount_3 = db.Column(db.Integer, nullable=False)
+    weather = db.Column(db.Text, nullable=True)
+    mood = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    @staticmethod
+    def newest(num):
+        return Bookmark.query.order_by(desc(Bookmark.date)).limit(num)
+
+    def __repr__(self):
+        return "<Bookmark '{}': '{}'>".format(self.amount, self.food, self)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    bookmarks = db.relationship('Bookmark', backref='user', lazy='dynamic')
+    password_hash = db.Column(db.String)
+
+    @property
+    def password(self):
+        raise AttributeError('password: write-only field')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @staticmethod
+    def get_by_username(username):
+        return User.query.filter_by(username=username).first()
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
---
+++ hold-files/hold/views.py
@@ -0,0 +1,91 @@
+from flask import render_template, flash, redirect, url_for, abort, request
+from flask_login import login_required, login_user, logout_user, current_user
+
+from hold import app, db, login_manager
+from forms import BookmarkForm, LoginForm, SignupForm
+from models import User, Bookmark
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.query.get(int(userid))
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', new_bookmarks=Bookmark.newest(5))
+
+
+@app.route('/add', methods=['GET', 'POST'])
+@login_required
+def add():
+    form = BookmarkForm()
+    if form.validate_on_submit():
+        food_word_1 = form.food_word_1.data
+        food_word_2 = form.food_word_2.data
+        food_word_3 = form.food_word_3.data
+        amount_1 = form.amount_1.data
+        amount_2 = form.amount_2.data
+        amount_3 = form.amount_3.data
+        weather = form.weather.data
+        mood = form.mood.data
+        bm = Bookmark(user=current_user, food_word_1=food_word_1, food_word_2=food_word_2, food_word_3=food_word_3, amount_1=amount_1, amount_2=amount_2,amount_3=amount_3,weather=weather, mood=mood)
+        db.session.add(bm)
+        db.session.commit()
+#        flash("Stored '{}'".format(food_word_1, food_word_2, food_word_3 ))
+        return redirect(url_for('index'))
+    return render_template('add.html', form=form)
+
+
+
+@app.route('/user/<username>')
+def user(username):
+
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.get_by_username(form.username.data)
+        if user is not None and user.check_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            flash("Logged in successfully as {}.".format(user.username))
+            return redirect(request.args.get('next') or url_for('user', username=user.username))
+        flash('Incorrect username or password.')
+    return render_template("login.html", form=form)
+
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Welcome, {}! Please login.'.format(user.username))
+        return redirect(url_for('login'))
+    return render_template("signup.html", form=form)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
